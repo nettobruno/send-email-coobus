b74e429220d15b7e673730215ad81cbe755f0024
Bruno Netto <brunonettomac@outlook.com>
2021-09-02 14:15:18 -0300

    Alterando projeto para ficar de acordo com o envio de E-mail da CooBus

--- src/app/controllers/SendEmailController.js
+++ src/app/controllers/SendEmailController.js
@@ -3,25 +3,44 @@ require('dotenv/config');
 
 class SendEmailController {
   async store(req, res) {
-    const { name, email, text } = req.body;
+    const { name, lastName, email, state, phone, message, date } = req.body;
 
     if (!name) {
       return res.status(400).json({ message: 'Nome não informado' });
+    }
+
+    if (!lastName) {
+      return res.status(400).json({ message: 'Sobrenome não informado' });
     }
 
     if (!email) {
       return res.status(400).json({ message: 'Email não informado' });
     }
 
-    if (!text) {
+    if (!state) {
+      return res.status(400).json({ message: 'Estado não informado' });
+    }
+
+    if (!phone) {
+      return res.status(400).json({ message: 'Número de celular não informado' });
+    }
+
+    if (!message) {
       return res
         .status(400)
-        .json({ message: 'Conteúdo do texto não informado' });
+        .json({ message: 'Conteúdo da mensagem não informado' });
+    }
+
+    if (!date) {
+      return res
+        .status(400)
+        .json({ message: 'Data não informada' });
     }
 
     var transporter = nodemailer.createTransport({
-      host: process.env.HOST,
-      secureConnection: false,
+      host: process.env.HOST,      
+      service: 'Outlook',
+      secure: false,         
       port: process.env.PORT,
       auth: {
         user: process.env.EMAIL,
@@ -30,26 +49,42 @@ class SendEmailController {
       tls: {
         ciphers: 'SSLv3',
       },
+      requireTLS: true,
     });
 
     let mailOptions = {
-      from: '"Contato pelo Portfólio " <' + process.env.EMAIL + '>',
+      from: '"Contato CooBUS " <' + process.env.EMAIL + '>',
       to: process.env.EMAIL,
-      subject: name,
-      html:
-        '<b>Nome: </b>' +
-        name +
-        '<br><b>Email: </b>' +
-        email +
-        '<br><b>Texto: </b>' +
-        text,
+      subject: 'Contato CooBUS',
+      html: '<b>Nome:</b> ' + name + ' ' + lastName +
+            '<br><b>E-mail:</b> ' + email + 
+            '<br><b>Estado:</b> ' + state + 
+            '<br><b>Celular:</b> ' + phone + 
+            '<br><b>Mensagem:</b> ' + message +
+            '<br><b>Data:</b> ' + date 
     };
 
-    transporter.sendMail(mailOptions, function (error, info) {
+    let mailOptionsClient = {
+      from: '"Contato CooBUS " <' + process.env.EMAIL + '>',
+      to: email,
+      subject: 'Contato CooBUS',
+      html: 'Recebemos sua mensagem. Aguarde, logo entraremos em contato :)'
+    };
+
+    await transporter.sendMail(mailOptions, (error) => {
+      transporter.sendMail(mailOptionsClient, (error) => {
+        if (error) {
+            return res
+            .status(400)
+            .json({ message: 'Algum erro ocorreu, tente novamente mais tarde'  });
+        }
+      });
+
+
       if (error) {
         return res
           .status(400)
-          .json({ message: 'Algum erro ocorreu, tente novamente mais tarde' });
+          .json({ message: 'Algum erro ocorreu, tente novamente mais tarde'  });
       }
 
       return res.status(200).json({ message: 'Email enviado com sucesso' });
--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 require('dotenv/config');
 
 app.listen(process.env.PORT_HOST || 3333, () => {
-  console.log('A API do send email está funcionando corretamente✅');
+  console.log('A API de envio de email do cooBus está funcionando corretamente✅');
 });
